ec804e8d927bcf75a399d7df75f230f0f5ab7ddf
NightlyKnight <116131829+thinknightly@users.noreply.github.com>
2023-01-04 00:06:02 +0700

    Add header.php and some components

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Import Header (Include CSS and JS)
+include('assets/components/index/header.php');
+
+// Import Public Dashboard
+include('assets/components/index/dashboard_public.php');
+?>
