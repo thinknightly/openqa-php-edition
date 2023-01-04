63c6f86fa92dacb22909c457a102ec77d4800539
NightlyKnight <116131829+thinknightly@users.noreply.github.com>
2023-01-04 15:14:45 +0700

    Update index.php
    เพิ่ม Description ใน index.php ให้เกียรติกับเจ้าของความคิดสร้างสรรค์ด้วยนะครับ 🙏

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-// Import Header (Include CSS and JS)
+// Setting SEO for Google Search
 echo ("<title>หน้าแรก - openQA</title>");
+echo ("<meta name='description' content='Development by thinknightly & pradukfuu!!. 
+ซอฟต์แวร์นี้พัฒนาให้งานประกันคุณภาพและมาตรฐานการศึกษาให้วิทยาลัยอาชีวศึกษาสุพรรณบุรี
+และขอขอบคุณความคิดสร้างสรรค์ทาง qa.cvc.ac.th ของวิทยาลัยอาชีวศึกษาเชียงรายด้วยนะครับ'>");
+
+// Import Header (Include CSS and JS)
 include('assets/components/index/header.php');
 
 // Import Navigator Bar
