ddbbfd0ee7b12c8368bf8661deca275c6bcd2257
NightlyKnight <116131829+thinknightly@users.noreply.github.com>
2023-01-04 11:51:35 +0700

    Setup Index Pages

---
+++ assets/components/index/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="assets/images/logo/openqa-php-edition.svg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+      openQA - <i class="fa-brands fa-php"></i> Edition
+    </a>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 // Import Header (Include CSS and JS)
 include('assets/components/index/header.php');
 
+// Import Navigator Bar
+include('assets/components/index/navbar.php');
 // Import Public Dashboard
 include('assets/components/index/dashboard_public.php');
+
+
 ?>
