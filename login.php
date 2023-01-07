1cea21af0b007f571d609ec1e2cd7bac842bd04f
NightlyKnight <116131829+thinknightly@users.noreply.github.com>
2023-01-07 12:18:02 +0700

    Update login.php

--- login.php
+++ login.php
@@ -24,13 +24,13 @@ include('assets/components/index/navbar.php');
                 <label for="formGroupExampleInput" class="form-label">
                     ชื่อผู้ใช้งาน
                 </label>
-                <input class="form-control" name="opqa_username" required />
+                <input type="text" class="form-control" name="opqa_username" required />
             </div>
             <div class="mt-2 mb-3">
                 <label for="formGroupExampleInput" class="form-label">
                     รหัสผ่าน
                 </label>
-                <input class="form-control" name="opqa_password" required />
+                <input type="password" class="form-control" name="opqa_password" required />
             </div>
             <input class="form-control mb-2 btn btn-success" type="submit" value="เข้าสู่ระบบ" />
         </form>
